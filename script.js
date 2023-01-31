a4e0e9e84546a40ea04609b8758a1bcb505ba27b
GillesP-dev <117928509+GillesP-dev@users.noreply.github.com>
2023-01-31 19:32:38 +0100

    rajout page galerie

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-let dernierePeinture = document.querySelector("#dernierePeinture1");
+const dernierePeinture = document.querySelector("#dernierePeinture1");
+const prenomAvis = document.querySelector("#prenom");
+let champPrenomAvis;
+const avisAvis = document.querySelector("#avis");
+let champAvis;
+const btnAvis = document.querySelector(".btnAvis");
+const affichageAvis = document.querySelector(".affichageAvis")
+
 
 dernierePeinture.addEventListener("click", ()=> {
    openFullscreen(); 
@@ -12,3 +19,24 @@ function openFullscreen() {
     dernierePeinture.msRequestFullscreen();
   }
 } 
+btnAvis.addEventListener("click", (event) => {
+  event.preventDefault();
+  recupAvis();
+  
+  
+})
+
+function recupAvis() {
+  champPrenomAvis = prenomAvis.value;
+  champAvis = avisAvis.value;
+  const titreAvis = document.createElement("h4")
+  titreAvis.textContent = champPrenomAvis;
+  affichageAvis.append(titreAvis);
+  const paraAvis = document.createElement("p");
+  paraAvis.textContent = champAvis;
+  affichageAvis.append(paraAvis);
+  prenomAvis.value = "";
+  avisAvis.value = "";
+
+}
+//----------------------- carousel -----------------------------//
